c852c6ac1140498577b3a0e00d430c4e6f82af5a
Mario <mariosent@gmail.com>
2025-12-13 18:22:37 +0300

    cocktail price section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -79,7 +79,7 @@ const Hero = () => {
         <p className='subtitle'>Sip the Spirit <br /> of Summer</p>
             </div>
             <div className="view-cocktails">
-                <p>Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed to delight your senses. </p>
+                <p className='subtitle'>Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed to delight your senses. </p>
                 <a href="#cocktails">View cocktails</a>
 
             </div>
